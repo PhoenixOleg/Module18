9bdd97f6cc49b4e18003b6e6aae0a3435a2a4ba6
PhoenixOleg <metall_2000@mail.ru>
2023-12-14 16:17:55 +0300

    Сделан класс Config для проверки и хранения конфигурации (URL, каталог для файлов)

---
+++ Module18/Config.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module18
+{
+    internal class Config
+    {
+        private bool _res = false;
+        private string _urlVideo;
+        private string _downloadPath;
+
+        internal string UrlVideo 
+        { 
+          get
+            {
+                return _urlVideo;
+            }
+          set
+            {
+                _urlVideo = value;
+                _res = false;
+            }
+        }
+
+        internal string DownloadPath 
+        { 
+            get
+            {
+                return _downloadPath;
+            }
+            set
+            {
+                _downloadPath = value;
+                _res = false;
+            }
+        }
+        
+        internal bool ValidatePassed 
+        {
+            get 
+            {
+                return _res;
+            }
+        }
+
+        internal Config() { }
+
+        internal Config(string UrlVideo, string DownloadPath) 
+        {
+            _urlVideo = UrlVideo.Trim();
+            _downloadPath = DownloadPath.Trim();
+
+            _res = Validator();
+        }
+
+        internal bool Validator()
+        {
+            if (string.IsNullOrEmpty(_urlVideo)) 
+            {
+                Console.ForegroundColor = ConsoleColor.Red; 
+                Console.WriteLine("URL видео не может быть пустым");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                _res = false;
+            }
+            else
+            {
+                Console.WriteLine($"URL видео для загрузки {_urlVideo}\n");
+            }
+
+            if (string.IsNullOrEmpty(_downloadPath) || !Directory.Exists(_downloadPath))
+            {
+                string myVideo = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos);
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Каталог для сохранения видео не может быть пустым и должен быть доступен");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                _downloadPath = myVideo;
+                Console.WriteLine($"Установлен каталог по-умолчанию {myVideo}\n");
+                _res = true;
+            }
+            else
+            {
+                Console.WriteLine($"Каталог для сохранения видео {_downloadPath}\n");
+            }
+
+            _res = true;
+            return _res;
+        }
+    }
+}
--- Module18/Program.cs
+++ Module18/Program.cs
@@ -4,6 +4,23 @@
     {
         static void Main(string[] args)
         {
+            Config myConfig = new();
+
+            if (args.Length != 2) 
+            {
+                Console.WriteLine("Некорректно заданы аргументы командной строки");
+            }
+            else
+            {
+                myConfig = new(args[0], args[1]);
+            }
+        
+            if (myConfig.ValidatePassed == false)
+            {
+                Console.WriteLine("Некорректно заданы параметры работы приложения");
+            }
+
+            Console.ReadKey();
 
         }
     }
